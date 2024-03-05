5a1b3052f17c6065137cddf27f73adb4f8b5a994
Ayaam <ayaam.mit@gmail.com>
2024-03-05 19:00:57 +0530

    React_Blog_App_Complete

---
+++ src/useFetch.js
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [isLoading, setisLoading] = useState(true);
+
+  useEffect(() => {
+    const abortCont = new AbortController();
+
+    fetch(url, { signal: abortCont.signal })
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("Could not fetch the data");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setData(data);
+        setisLoading(false);
+        setError(null);
+      })
+      .catch((err) => {
+        if (err.name === "AbortError") {
+          console.log("fetch aborted");
+        } else {
+          setisLoading(false);
+          setError(null);
+        }
+      });
+
+    return () => abortCont.abort();
+  }, [url]);
+
+  return { data, error, isLoading };
+};
+
+export default useFetch;
